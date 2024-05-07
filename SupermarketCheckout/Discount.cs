cb0a17076f0cd9096cdec251b0fa90015f41e995
Neil Butler <neilbutleranalytics@outlook.com>
2024-05-07 10:43:01 +0100

    Lets create 2 classes for our packages and discounts

---
+++ SupermarketCheckout/Discount.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SupermarketCheckout
+{
+    public class Discount
+    {
+        public Discount (string sku, int numberOfItems, int priceOfPackage)
+        {
+            this.Sku = sku;
+            this.NumberOfItems = numberOfItems;
+            this.PackagePrice = priceOfPackage;
+        }
+
+        public readonly string Sku;
+        public readonly int NumberOfItems;
+        public readonly int PackagePrice;
+    }
+
+    public class Discounts
+    {
+        private List<Discount> discounts = new List<Discount>();
+
+    }
+}
