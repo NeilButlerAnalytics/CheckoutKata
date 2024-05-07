e7a00c36347f4336161745caff2b1f36f60e935e
Neil Butler <neilbutleranalytics@outlook.com>
2024-05-07 11:17:03 +0100

    Include more tests now that we have created a working structure

--- SupermarketCheckout.Tests/CheckoutTests.cs
+++ SupermarketCheckout.Tests/CheckoutTests.cs
@@ -13,32 +13,77 @@ namespace SupermarketCheckout.Tests
                     .UpdateProductPrice("B", 30)
                     .UpdateProductPrice("C", 20)
                     .UpdateProductPrice("D", 15);
-            // INCLUDE OUR NEW DISCOUNTS CATALOG AND SET THE PRICES OF OUR DISCOUNTS
+
             var discounts = new Discounts()
                     .DiscountProduct("A", 3, 130)
                     .DiscountProduct("B", 2, 45);
-            checkout = new Checkout();
+            checkout = new Checkout(catalog, discounts);
         }
 
         Checkout checkout;
         [Fact]
         public void true_FindPriceOfItemA()
-        {
-            // Assume we create an object called 'checkout' as part of our code                                               
+        {                                      
             checkout.Scan("A");
             
-            // within checkout we will retrieve the price of the product and compare
             Assert.Equal(50, checkout.Total);
         }
 
         [Fact]
         public void trueFindPriceOfItemB()
-        {
-            // Assume we create an object called 'checkout' as part of our code                                                           
+        {                                                        
             checkout.Scan("B");
-
-            // within checkout we will retrieve the price of the product and compare                                                         
+            
             Assert.Equal(30, checkout.Total);
+        }
+
+        // This method is a parameterized test executed multiple times
+        [Theory]
+        [InlineData('A', 50)]
+        [InlineData('B', 30)]
+        [InlineData('C', 20)]
+        [InlineData('D', 15)]
+        public void trueFindPriceOfItem(string sku, int expected_total)
+        {                                                       
+            checkout.Scan(sku);
+                                                            
+            Assert.Equal(expected_total, checkout.Total);
+        }
+
+        [Theory]
+        [InlineData('A', 2, 100)]
+        [InlineData('B', 1, 30)]
+        [InlineData('C', 10, 200)]
+        [InlineData('D', 5, 75)]
+        public void trueFindPriceOfMultiples(string sku, int number_of_items, int expected_total)
+        {                                                             
+            checkout.Scan(sku, number_of_items);
+                                                           
+            Assert.Equal(expected_total, checkout.Total);
+        }
+
+        [Theory]
+        [InlineData('A', 3, 130)]
+        [InlineData('B', 2, 45)]
+        [InlineData('A', 7, 310)]
+        [InlineData('B', 11, 255)]
+        public void trueCalculateDiscounts(string sku, int number_of_items, int expected_total)
+        {                                                         
+            checkout.Scan(sku, number_of_items);
+                                                           
+            Assert.Equal(expected_total, checkout.Total);
+        }
+
+        [Theory]
+        [InlineData("AABCCDC", 205)]
+        public void trueFindPriceForStream(string stream, int expected_total)
+        {
+            foreach (var sku in stream)
+            {
+                checkout.Scan("" + sku);
+            }
+
+            Assert.Equal(expected_total, checkout.Total);
         }
 
 
